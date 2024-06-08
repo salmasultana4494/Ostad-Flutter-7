00098e8663237025c512351512438bb2d2263e3b
salma sultana <salmasultana4494@gmail.com>
2024-06-08 13:17:34 +0600

    Test

---
+++ Module-1/Class-3/Constant.dart
@@ -0,0 +1,10 @@
+main(){
+  const String university = 'United International University';
+  const age = 12;
+  print(age);
+  print(age.runtimeType);
+  final double gpa;
+  gpa = 4.60;
+  print(gpa);
+  print(gpa.runtimeType);
+}
---
+++ Module-1/Class-3/Var-Dynamic.dart
@@ -0,0 +1,14 @@
+main(){
+  String name = 'salma';
+  print(name);
+  var cgpa = 3.00;
+  print(cgpa);
+  print(cgpa.runtimeType);
+  cgpa = 3.31;
+  print(cgpa);
+
+  dynamic random = 3;
+  print(random);
+  random = 'hello';
+  print(random);
+}
